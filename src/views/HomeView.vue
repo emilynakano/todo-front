143d1b3c990e1e7e1ee0a32d6d2584ac5ae983aa
emily <nakanodepaula@gmail.com>
2023-01-11 10:12:26 -0300

    feat: greeting and name persistency

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch, onMounted } from "vue";
+
+const name = ref("");
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
+</script>
 
 <template>
-  <main>
-    <h1>todo</h1>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up <input type="text" placeholder="Your name" v-model="name" />
+      </h2>
+    </section>
   </main>
 </template>
